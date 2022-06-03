9cb49947b81aff8d427b6367656c3d84c01da20a
ReallyCoolHackerGirl <92086872+abban04@users.noreply.github.com>
2022-06-03 20:42:36 +0200

    Add files via upload

---
+++ m05e1.py
@@ -0,0 +1,4 @@
+def me():
+    print("My name is Albert, and I am in the class TE20D at NTI Kronhus")
+
+me()
---
+++ m05e2.py
@@ -0,0 +1,11 @@
+def circumference():
+    radie = int(input("What's the radius"))
+    print(2*3.14*(radie))
+
+def area():
+    radie = int(input("What's the radius"))
+    print((radie*radie)*3.14)
+
+
+circumference()
+area()
